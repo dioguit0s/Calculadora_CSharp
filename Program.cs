82e5356d097008dec28a7303c67cf9c6e2fbe2a3
dioguit0s <diogosantos152005@gmail.com>
2024-03-03 21:45:09 -0300

    Adicionado comand TryCatch em possiveis erros de digitação

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ namespace Calculadora
 
                 opcao = int.Parse(Console.ReadLine());
 
-                //Verifica se a opção selecionada foi sair
+                //Verifica se a opção selecionada foi sair 
                 if (opcao == 0)
                 {
                     //chama o método que fecha a aplicação caso 
@@ -49,26 +49,42 @@ namespace Calculadora
                 
                 else if (opcao > 5) //Verifica se o número digitad foi maior que o esperado e repete a operação para o usuario tentar novamente
                 {
+                    Console.ForegroundColor= ConsoleColor.Red;
                     Console.WriteLine("Operação invalida, por favor pressione qualquer botão para tentar novamente");
+                    Console.ForegroundColor= ConsoleColor.Gray;
                     Console.ReadKey();
                     Console.Clear();
                     CalculoPrincipal();
                 }
 
                 //Leitura dos números digitados pelo usuario
-                
                 try 
                 {
                 Console.WriteLine("Digite o primeiro número: ");
                 num1 = double.Parse(Console.ReadLine());
-                } catch (ArgumentException) 
+                } catch (FormatException) 
                 {
+                    Console.ForegroundColor= ConsoleColor.Red;
                     Console.WriteLine("O item digitado não foi um número, tente novamente");
-                    throw;
+                    Console.ForegroundColor= ConsoleColor.Gray;
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    CalculoPrincipal();
                 }
+
+                try{
                 Console.WriteLine("Digite o secundo número: ");
                 num2 = double.Parse(Console.ReadLine());
-
+                } catch (FormatException) 
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("O item digitado não foi um número, tente novamente");
+                    Console.ForegroundColor= ConsoleColor.Gray;
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    CalculoPrincipal();
+                }
+                
                 //função switchcase para realização das operações
                 switch (opcao)
                 {
@@ -85,8 +101,22 @@ namespace Calculadora
                         break;
 
                     case 4:
-                        resultado = num1 / num2;
-                        break;
+                    try
+                    {
+                        resultado = num1 / num2;                       
+                    }
+                    catch (DivideByZeroException) //Essa exceção deveria evitar divisões por zero porém a linguagem reconhece divisões por zero como um número infinito
+                                                  //então estará aqui apenas de enfeite pois usando numeros reais nunva irá dar um erro de divisão por 0
+                    {
+                        Console.ForegroundColor= ConsoleColor.Red;
+                        Console.WriteLine("Não é possivel dividir por zero, escolha outra operação");
+                        Console.ForegroundColor= ConsoleColor.Gray;
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        CalculoPrincipal();
+
+                    }
+                    break;
 
                     case 5:
                         resultado = num1 % num2;
@@ -98,11 +128,20 @@ namespace Calculadora
                 //método que verifica se o usuario deseja realizar outro calculo
                  void Repetir()
                 {
-                    int repeticao;
+                    int repeticao = 0;
 
+                    try {
                     Console.WriteLine("Deseja realizar outro cálculo?\n" + "Digite 1 para sim e 2 para não");
                     repeticao = int.Parse(Console.ReadLine());
-
+                    } catch (FormatException) 
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("O item digitado não foi um número, tente novamente");
+                    Console.ForegroundColor= ConsoleColor.Gray;
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Repetir();
+                }
                     if (repeticao == 1)
                     {
                         //limpa o console e cria novamente o menu principal
@@ -112,7 +151,9 @@ namespace Calculadora
                     else if (repeticao == 2) Fechar();
                     else
                     {
+                        Console.ForegroundColor= ConsoleColor.Red;
                         Console.WriteLine("Comando inválido, fechando aplicação");
+                        Console.ForegroundColor= ConsoleColor.Gray;
                         Fechar();
                     }
 
